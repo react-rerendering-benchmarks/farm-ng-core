f6e26f7c6733170cce9c99fcab150024ad4c6adc
Satyajit Gokhale <twisterintwist@gmail.com>
2024-10-13 10:14:30 -0400

    Instruenting React Components

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
-
 type FeatureItem = {
   title: string;
   description: JSX.Element;
 };
-
-const FeatureList: FeatureItem[] = [
-  {
-    title: 'Core layer',
-    description: (
-      <>
+const FeatureList: FeatureItem[] = [{
+  title: 'Core layer',
+  description: <>
         Foundational core layer for new c++ and python projects.
 
         Convenient macros such as FARM_ENUM, FARM_ASSERT_* and FARM_UNWRAP.
@@ -22,52 +18,51 @@ const FeatureList: FeatureItem[] = [
         (both being targeted for c++ standardization) and Protocol Buffers /
         GRPC.
       </>
-    ),
-  },
-  {
-    title: 'sophus2: 2d / 3d Geometry c++ Library',
-    description: (
-      <>
+}, {
+  title: 'sophus2: 2d / 3d Geometry c++ Library',
+  description: <>
       Lie Groups / Manifolds commonly used for 2d / 3D geometric problems.
       Image Classes: Image, MutImage, DynImage, MutDynImage and view classes.
       Collection of camera models (pinhole, brown-conrady aka opencv,
       kannala-brandt and orthographic), IMU mode and more. Python bindings
       are available for a subset of the library.
       </>
-    ),
-  },
-  {
-    title: 'Multi-language serialization and logging utilities',
-    description: (
-      <>
+}, {
+  title: 'Multi-language serialization and logging utilities',
+  description: <>
         EventLogReader and EventLogWriter to create and playback Protocol
         Buffer-based datasets and some more general purpose c++/python utilities.
       </>
-    ),
-  },
-];
-
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
+}];
+function Feature({
+  title,
+  Svg,
+  description
+}: FeatureItem) {
+  console.log(window.globalCount++);
+  return <div className={clsx('col col--4')}>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
-    </div>
-  );
+    </div>;
 }
-
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
+  console.log(window.globalCount++);
+  return <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+          {FeatureList.map((props, idx) => {
+          console.log(window.globalCount++);
+          return <Feature key={idx} {...props} />;
+        })}
         </div>
       </div>
-    </section>
-  );
+    </section>;
 }
+declare global {
+  interface Window {
+    globalCount: number;
+  }
+}
+window.globalCount = 0;
